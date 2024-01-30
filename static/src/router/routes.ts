e66f1b28d1639c40833738739b2dc4e5650421a8
sunlijie <982268620@qq.com>
2024-01-30 16:18:33 +0800

    feat:搭建表单页的基础结构

--- static/src/router/routes.ts
+++ static/src/router/routes.ts
@@ -1,9 +1,6 @@
 import BasicLayout from '@/layouts/BasicLayout.vue'
 import RouteView from '@/layouts/RouteView.vue'
-import {
-  FieldTimeOutlined,
-  HeartOutlined,
-} from '@ant-design/icons-vue';
+import { FieldTimeOutlined, HeartOutlined, FormOutlined } from '@ant-design/icons-vue'
 export const routes = [
   {
     path: '/',
@@ -15,7 +12,7 @@ export const routes = [
         path: '/workplace',
         name: 'workplace',
         meta: { title: '工作台', icon: FieldTimeOutlined },
-        component: () => import('@/views/workplace/Workplace.vue'),
+        component: () => import('@/views/workplace/Workplace.vue')
       },
       {
         path: '/dashboard',
@@ -28,22 +25,49 @@ export const routes = [
             path: '/dashboard/welcome',
             name: 'welcome',
             meta: { title: '欢迎页', icon: FieldTimeOutlined },
-            component: () => import('@/views/dashboard/Welcome.vue'),
+            component: () => import('@/views/dashboard/Welcome.vue')
           },
           {
             path: '/dashboard/analysis',
             name: 'analysis',
             meta: { title: '分析页', icon: HeartOutlined },
-            component: () => import('@/views/dashboard/Analysis.vue'),
+            component: () => import('@/views/dashboard/Analysis.vue')
           },
           {
             path: '/dashboard/monitor',
             name: 'monitor',
             meta: { title: '监控页', icon: HeartOutlined },
-            component: () => import('@/views/dashboard/Monitor.vue'),
+            component: () => import('@/views/dashboard/Monitor.vue')
+          }
+        ]
+      },
+      {
+        path: '/form',
+        name: 'form',
+        redirect: '/form/basic-form',
+        component: RouteView,
+        meta: { title: '表单页', icon: FormOutlined },
+        children: [
+          {
+            path: '/form/basic-form',
+            name: 'basic',
+            meta: { title: '基础表单' },
+            component: () => import('@/views/form/BasicForm.vue')
           },
+          {
+            path: '/form/step-form',
+            name: 'step',
+            meta: { title: '分步表单' },
+            component: () => import('@/views/form/StepForm.vue')
+          },
+          {
+            path: '/form/advanced-form',
+            name: 'advanced',
+            meta: { title: '高级表单' },
+            component: () => import('@/views/form/AdvancedForm.vue')
+          }
         ]
       }
     ]
   }
-];
+]
---
+++ static/src/views/form/AdvancedForm.vue
@@ -0,0 +1,8 @@
+<template>
+  <page-view>
+
+  </page-view>
+</template>
+<script lang="ts" setup>
+import PageView from '@/layouts/PageView.vue';
+</script>
